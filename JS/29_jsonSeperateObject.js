59bced0fb4c53553db2796364e3a9bdc2bdd9a51
Seyed <iammseyed@proton.me>
2025-08-08 08:54:43 +0530

    added web basics

---
+++ JS/29_jsonSeperateObject.js
@@ -0,0 +1,9 @@
+let schoolJson ={
+    "schoolObject":
+    [
+         {"schoolName":"name0","schoolAge":6,"schoolLocation":"SXR"},
+         {"schoolName":"name1","schoolAge":7,"schoolLocation":"BGM"},
+         {"schoolName":"name2","schoolAge":8,"schoolLocation":"BLA"},
+    ]
+}
+export {schoolJson}; 
